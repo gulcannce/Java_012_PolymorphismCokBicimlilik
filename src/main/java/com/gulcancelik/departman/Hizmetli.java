940969eb2c236efdd20b6fd612fa9cb6cd48fb82
gulcannce <gulcanncee@gmail.com>
2024-11-09 20:09:34 +0300

    Initial commit

---
+++ src/main/java/com/gulcancelik/departman/Hizmetli.java
@@ -0,0 +1,15 @@
+package com.gulcancelik.departman;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hizmetli extends Muhasebeci{
+
+  private  String vazifesi;
+}
